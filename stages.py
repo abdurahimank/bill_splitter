0642dd12ef9ca40360e093a0a5cc07888fbba810
ABDURAHIMAN K <laluvga@gmail.com>
2023-03-09 10:36:54 +0530

    stage 2/4)

--- stages.py
+++ stages.py
@@ -1,9 +1,10 @@
-# Stage 1/4: Invite your friends
+# Stage 2/4: The bill has arrived
 no_people = int(input("Enter the number of friends joining (including you):\n"))
 if no_people <= 0:
     print("\nNo one is joining for the party")
 else:
     print("\nEnter the name of every friend (including you), each on a new line:")
     people = [input() for i in range(no_people)]
-    bill = dict.fromkeys(people, 0)
+    bill_value = int(input("\nEnter the total bill value:\n"))
+    bill = dict.fromkeys(people, round(bill_value / no_people, 2))
     print("\n", bill, sep="")
